1f07978544d1e78c140bb133f76c9e2c0bc87471
Artur_Laptop <ArchieBar@yandex.ru>
2023-12-02 00:51:04 +0300

    fix: Исправить тестовый класс InMemoryHistoryManager

--- test/managerTest/InMemoryHistoryManagerTest.java
+++ test/managerTest/InMemoryHistoryManagerTest.java
@@ -2,54 +2,157 @@ package managerTest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tasksTest.EpicTask;
+import tasksTest.SubTask;
 import tasksTest.Task;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class InMemoryHistoryManagerTest {
 
     @Test
-    public void CheckingForCorrectnessOfAddingATaskToHistoryList() throws ManagerSaveException {
-        TaskManager taskManager = new InMemoryTaskManager();
-        Task task1 = new Task("Task", "Description",
+    public void testAddHistoryShouldAddToHistoryWhenTaskNotNull() throws ManagerSaveException {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Task task = new Task("Task", "Description",
                 LocalDateTime.of(2020, 10, 10, 10, 0), Duration.ofMinutes(15));
-        Task task2 = new Task("Task", "Description",
-                LocalDateTime.of(2021, 10, 10, 10, 0), Duration.ofMinutes(15));
-        Task task3 = new Task("Task", "Description",
+        task.setId(1);
+        EpicTask epicTask = new EpicTask("EpicTask", "Description");
+        epicTask.setId(2);
+        SubTask subTask = new SubTask("SubTask", "Description", epicTask.getId(),
                 LocalDateTime.of(2022, 10, 10, 10, 0), Duration.ofMinutes(15));
-        taskManager.createTask(task1);
-        taskManager.createTask(task2);
-        taskManager.createTask(task3);
-        taskManager.getTaskById(task1.getId());
-        taskManager.getTaskById(task2.getId());
-        taskManager.getTaskById(task3.getId());
+        subTask.setId(3);
 
-        assertEquals(taskManager.getHistory().size(), 3);
+        historyManager.addHistory(task);
+        historyManager.addHistory(epicTask);
+        historyManager.addHistory(subTask);
+
+        int actualArea = historyManager.getHistory().size();
+        int expectedArea = 3;
+        assertEquals(expectedArea, actualArea);
+
+        Task actualTask = historyManager.getHistory().get(0);
+        Task expectedTask = task;
+        assertEquals(expectedTask, actualTask);
     }
 
     @Test
-    public void CheckingForCorrectnessOfDeletingATaskFromHistoryList() throws ManagerSaveException {
-        TaskManager taskManager = Manager.getDefault();
-        Task task1 = new Task("Task", "Description",
+    public void testAddHistoryDuplicateTaskShouldNotBeAtEndOfListAgainIfTaskIsNotNull() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+        Task task = new Task("Task", "Description",
                 LocalDateTime.of(2020, 10, 10, 10, 0), Duration.ofMinutes(15));
-        Task task2 = new Task("Task", "Description",
-                LocalDateTime.of(2021, 10, 10, 10, 0), Duration.ofMinutes(15));
-        Task task3 = new Task("Task", "Description",
+        task.setId(1);
+        EpicTask epicTask = new EpicTask("EpicTask", "Description");
+        epicTask.setId(2);
+        SubTask subTask = new SubTask("SubTask", "Description", epicTask.getId(),
                 LocalDateTime.of(2022, 10, 10, 10, 0), Duration.ofMinutes(15));
-        taskManager.createTask(task1);
-        taskManager.createTask(task2);
-        taskManager.createTask(task3);
-        taskManager.getTaskById(task1.getId());
-        taskManager.getTaskById(task2.getId());
-        taskManager.getTaskById(task3.getId());
+        subTask.setId(3);
 
-        assertEquals(taskManager.getHistory().size(), 3);
+        historyManager.addHistory(task);
+        historyManager.addHistory(epicTask);
+        historyManager.addHistory(subTask);
+        historyManager.addHistory(task);
 
-        taskManager.removeAllTask();
+        int actualSize = historyManager.getHistory().size();
+        int expectedSize = 3;
+        assertEquals(expectedSize, actualSize);
 
-        assertEquals(taskManager.getHistory().size(), 0);
+        Task actualTask = historyManager.getHistory().get(2);
+        Task expectedTask = task;
+        assertEquals(expectedTask, actualTask);
     }
 
+    @Test
+    public void testAddHistoryAnErrorShouldNotOccurWhenTaskIsNullAndHistoryListShouldNotChange() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+        Task task = new Task("Task", "Description",
+                LocalDateTime.of(2020, 10, 10, 10, 0), Duration.ofMinutes(15));
+        task.setId(1);
+        EpicTask epicTask = new EpicTask("EpicTask", "Description");
+        epicTask.setId(2);
+        SubTask subTask = new SubTask("SubTask", "Description", epicTask.getId(),
+                LocalDateTime.of(2022, 10, 10, 10, 0), Duration.ofMinutes(15));
+        subTask.setId(3);
+
+        historyManager.addHistory(task);
+        historyManager.addHistory(epicTask);
+        historyManager.addHistory(subTask);
+        historyManager.addHistory(null);
+
+        int actualSize = historyManager.getHistory().size();
+        int expectedSize = 3;
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void testRemoveHistoryWhenDeletingAHistoryFromBeginningOfMiddleOfEndOfList() throws ManagerSaveException {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+        Task task1 = new Task("Task", "Description",
+                LocalDateTime.of(2020, 10, 10, 10, 0), Duration.ofMinutes(15));
+        task1.setId(1);
+        Task task2 = new Task("Task", "Description",
+                LocalDateTime.of(2020, 10, 10, 10, 0), Duration.ofMinutes(15));
+        task2.setId(2);
+        EpicTask epicTask = new EpicTask("EpicTask", "Description");
+        epicTask.setId(3);
+        SubTask subTask = new SubTask("SubTask", "Description", epicTask.getId(),
+                LocalDateTime.of(2022, 10, 10, 10, 0), Duration.ofMinutes(15));
+        subTask.setId(4);
+
+        historyManager.addHistory(task1);
+        historyManager.addHistory(task2);
+        historyManager.addHistory(epicTask);
+        historyManager.addHistory(subTask);
+
+        int actualSize = historyManager.getHistory().size();
+        int expectedSize = 4;
+        assertEquals(expectedSize, actualSize);
+
+        historyManager.remove(task2.getId());
+        actualSize = historyManager.getHistory().size();
+        expectedSize = 3;
+        assertEquals(expectedSize, actualSize);
+
+        historyManager.remove(subTask.getId());
+        actualSize = historyManager.getHistory().size();
+        expectedSize = 2;
+        assertEquals(expectedSize, actualSize);
+
+        historyManager.remove(task1.getId());
+        actualSize = historyManager.getHistory().size();
+        expectedSize = 1;
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void testRemoveHistoryWhenDeletingAHistoryByANonExistentID() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+        Task task = new Task("Task", "Description",
+                LocalDateTime.of(2020, 10, 10, 10, 0), Duration.ofMinutes(15));
+        task.setId(1);
+        EpicTask epicTask = new EpicTask("EpicTask", "Description");
+        epicTask.setId(2);
+        SubTask subTask = new SubTask("SubTask", "Description", epicTask.getId(),
+                LocalDateTime.of(2022, 10, 10, 10, 0), Duration.ofMinutes(15));
+        subTask.setId(3);
+
+        historyManager.addHistory(task);
+        historyManager.addHistory(epicTask);
+        historyManager.addHistory(subTask);
+
+        int actualSize = historyManager.getHistory().size();
+        int expectedSize = 3;
+        assertEquals(expectedSize, actualSize);
+
+        historyManager.remove(-1);
+        actualSize = historyManager.getHistory().size();
+        expectedSize = 3;
+        assertEquals(expectedSize, actualSize);
+    }
 }
